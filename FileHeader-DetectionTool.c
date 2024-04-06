41ccdcdacaec61c066ef6601a588c87a57cf0222
AdaptiveResonance <105830013+AdaptiveResonance@users.noreply.github.com>
2024-04-06 01:13:55 -0600

    Improved compatibility and formatting

--- FileHeader-DetectionTool.c
+++ FileHeader-DetectionTool.c
@@ -23,51 +23,51 @@ int main(int argc, char *argv[])
 	char file[30] = {0};
 	strcpy(file, argv[1]);
 	FILE *fileptr; // file pointer 
-    fileptr = fopen(file, "r");
-    if(fileptr == NULL)
-    {
-        fprintf(stderr, "there is an error in file path: %s\n", strerror(errno));
-        exit(-1);
-    }
-    char buffer[5];
-    fread(buffer, 1, 4, fileptr);
-    
-    if(buffer[0] == 0x31 && buffer[1] == 0x35 && buffer[2] == 0x30 && buffer[3] == 0x20)
-    {
-    	printf("im in testing diagnostic\n");//left this in for diagnostic
-    }
-    if(buffer[0] == 0x7F && buffer[1] == 0x45 && buffer[2] == 0x4C && buffer[3] == 0x46)
-    {
-    	printf("option 1 This is an ELF file\n");
-    }
-    if(buffer[0] == 0x59 && buffer[1] == 0x52 && buffer[2] == 0x41 && buffer[3] == 0x47)
-    {
-    	printf("option 2 This is a YRAG file\n");
-    }
-        if(buffer[0] == 0x89 && buffer[1] == 0x50 && buffer[2] == 0x4E && buffer[3] == 0x47)
-    {
-    	printf("option 2 This is a png file\n");
-    }
-        if(buffer[0] == 0xFF && buffer[1] == 0xD8 && buffer[2] == 0xFF)
-    {
-    	printf("option 2 This is a jpeg file\n");
-    }
-        if(buffer[0] == 0x25 && buffer[1] == 0x50 && buffer[2] == 0x44 && buffer[3] == 0x46)
-    {
-    	printf("option 2 This is a pdf file\n");
-    }
-        if(buffer[0] == 0x4D && buffer[1] == 0x5A)
-    {
-    	printf("option 2 This is a mz file\n");
-    }
+	fileptr = fopen(file, "r");
+	if(fileptr == NULL)
+	{
+		fprintf(stderr, "there is an error in file path: %s\n", strerror(errno));
+		exit(-1);
+	}
+	char buffer[5];
+	fread(buffer, 1, 4, fileptr);
+	
+	if(buffer[0] == 0x31 && buffer[1] == 0x35 && buffer[2] == 0x30 && buffer[3] == 0x20)
+	{
+		printf("im in testing diagnostic\n");//left this in for diagnostic
+	}
+	if(buffer[0] == 0x7F && buffer[1] == 0x45 && buffer[2] == 0x4C && buffer[3] == 0x46)
+	{
+		printf("option 1 This is an ELF file\n");
+	}
+	if(buffer[0] == 0x59 && buffer[1] == 0x52 && buffer[2] == 0x41 && buffer[3] == 0x47)
+	{
+		printf("option 2 This is a YRAG file\n");
+	}
+		if(buffer[0] == 0x89 && buffer[1] == 0x50 && buffer[2] == 0x4E && buffer[3] == 0x47)
+	{
+		printf("option 2 This is a png file\n");
+	}
+		if(buffer[0] == 0xFF && buffer[1] == 0xD8 && buffer[2] == 0xFF)
+	{
+		printf("option 2 This is a jpeg file\n");
+	}
+		if(buffer[0] == 0x25 && buffer[1] == 0x50 && buffer[2] == 0x44 && buffer[3] == 0x46)
+	{
+		printf("option 2 This is a pdf file\n");
+	}
+		if(buffer[0] == 0x4D && buffer[1] == 0x5A)
+	{
+		printf("option 2 This is a mz file\n");
+	}
 
-    fseek(fileptr, 0x3C, SEEK_SET);
-    fread(buffer, 1, 4, fileptr);
-    char bigbuff[41] = "\0";
-    fseek(fileptr, -4, SEEK_CUR);
-    fread(bigbuff, 1, 40, fileptr);
-    int num = atoi(buffer);
-    printf("The 4byte string at offset 0x3c is: %s in integer value it is:%d\n", buffer, num);
-    printf("The extended string at offset 0x3c is: %s\n", bigbuff);
+	fseek(fileptr, 0x3C, SEEK_SET);
+	fread(buffer, 1, 4, fileptr);
+	char bigbuff[41] = "\0";
+	fseek(fileptr, -4, SEEK_CUR);
+	fread(bigbuff, 1, 40, fileptr);
+	int num = atoi(buffer);
+	printf("The 4byte string at offset 0x3c is: %s in integer value it is:%d\n", buffer, num);
+	printf("The extended string at offset 0x3c is: %s\n", bigbuff);
 	decoder(bigbuff);
 }
